ad93fb5d52cd523a6784fb5e4107ce64bc78f93b
keshavrathinavel <keshavrathinavel@gmail.com>
2024-06-02 16:43:36 +0530

    Init setup

---
+++ src/app/page.tsx
@@ -0,0 +1,32 @@
+import SignInButton from "@/components/SignInButton";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+
+export default async function Home() {
+  const session = await getServerSession();
+  if (session?.user) {
+    redirect("/dashboard");
+  }
+  return (
+    <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+      <Card className="w-[300px]">
+        <CardHeader>
+          <CardTitle>Welcome to Saplings.AI 🌱</CardTitle>
+          <CardDescription>
+            Take an AI generated quiz now! Sign in below!
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SignInButton text="Sign In with Google" />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
