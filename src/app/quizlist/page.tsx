e3c3224949e2655e1ee8edcbeeb5b2b7f29ebf47
keshavrathinavel <keshavrathinavel@gmail.com>
2024-06-03 01:54:43 +0530

    Create card that will house the button which upon clicking will take user to QuizList

---
+++ src/app/quizlist/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import {useRouter} from "next/navigation";
+import React from "react";
+import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
+import {BookCopy} from "lucide-react";
+
+const ViewAvailableQuizzes = () => {
+
+    const router = useRouter();
+
+    return (
+        <Card
+            className="hover:cursor-pointer w-full hover:opacity-75"
+            onClick={() => {
+                router.push("/list");
+            }}
+        >
+            <CardHeader className="flex flex-row items-center justify-between">
+                <CardTitle className="text-2xl font-bold">View available quizzes</CardTitle>
+                <BookCopy size={28}/>
+            </CardHeader>
+            <CardContent>
+                <p className="text-sm text-muted-foreground">
+                    Take any of the available quizzes.
+                </p>
+            </CardContent>
+        </Card>
+
+    );
+};
+
+export default ViewAvailableQuizzes;
