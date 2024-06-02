a62ce25ef72073ea1a9409accfb898ce4e36d8cf
keshavrathinavel <keshavrathinavel@gmail.com>
2024-06-03 01:33:09 +0530

    Make sign in button occupy full width of login card

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -1,20 +1,21 @@
 "use client";
 import React from "react";
-import { Button } from "./ui/button";
-import { signIn } from "next-auth/react";
+import {Button} from "./ui/button";
+import {signIn} from "next-auth/react";
 
 type Props = { text: string };
 
-const SignInButton = ({ text }: Props) => {
-  return (
-    <Button
-      onClick={() => {
-        signIn("google").catch(console.error);
-      }}
-    >
-      {text}
-    </Button>
-  );
+const SignInButton = ({text}: Props) => {
+    return (
+        <Button
+            className="w-full"
+            onClick={() => {
+                signIn("google").catch(console.error);
+            }}
+        >
+            {text}
+        </Button>
+    );
 };
 
 export default SignInButton;
