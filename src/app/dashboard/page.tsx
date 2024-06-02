299a25765a3d813ad20c16603d26cac85ce9a9da
keshavrathinavel <keshavrathinavel@gmail.com>
2024-06-03 01:55:22 +0530

    Add card to view all available quizzes in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,40 +3,46 @@ import HistoryCard from "@/components/dashboard/HistoryCard";
 import HotTopicsCard from "@/components/dashboard/HotTopicsCard";
 import QuizMeCard from "@/components/dashboard/QuizMeCard";
 import RecentActivityCard from "@/components/dashboard/RecentActivityCard";
-import { getAuthSession } from "@/lib/nextauth";
-import { redirect } from "next/navigation";
+import {getAuthSession} from "@/lib/nextauth";
+import {redirect} from "next/navigation";
 import React from "react";
+import ViewAvailableQuizzes from "@/app/quizlist/page";
 
 type Props = {};
 
 export const metadata = {
-  title: "Dashboard | Saplings.AI",
-  description: "Take a quiz on anything!",
+    title: "Dashboard | Saplings.AI",
+    description: "Take a quiz on anything!",
 };
 
 const Dashboard = async (props: Props) => {
-  const session = await getAuthSession();
-  if (!session?.user) {
-    redirect("/");
-  }
+    const session = await getAuthSession();
+    if (!session?.user) {
+        redirect("/");
+    }
 
-  return (
-    <main className="p-8 mx-auto max-w-7xl">
-      <div className="flex items-center">
-        <h2 className="mr-2 text-3xl font-bold tracking-tight">Dashboard</h2>
-        <DetailsDialog />
-      </div>
+    return (
+        <main className="p-8 mx-auto max-w-7xl">
+            <div className="flex items-center">
+                <h2 className="mr-2 text-3xl font-bold tracking-tight">Dashboard</h2>
+                <DetailsDialog/>
+            </div>
 
-      <div className="grid gap-4 mt-4 md:grid-cols-2">
-        <QuizMeCard />
-        <HistoryCard />
-      </div>
-      <div className="grid gap-4 mt-4 md:grid-cols-2 lg:grid-cols-7">
-        <HotTopicsCard />
-        <RecentActivityCard />
-      </div>
-    </main>
-  );
+            <div className="grid gap-4 mt-4 md:grid-cols-2">
+                <QuizMeCard/>
+                <HistoryCard/>
+            </div>
+
+            <div className="grid gap-4 mt-4 md:grid-cols-1">
+                <ViewAvailableQuizzes/>
+            </div>
+
+            <div className="grid gap-4 mt-4 md:grid-cols-2 lg:grid-cols-7">
+                <HotTopicsCard/>
+                <RecentActivityCard/>
+            </div>
+        </main>
+    );
 };
 
 export default Dashboard;
