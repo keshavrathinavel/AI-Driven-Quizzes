5ff21dc5c2fb6db227ba1983e55f129846a38fd5
keshavrathinavel <keshavrathinavel@gmail.com>
2024-06-03 02:08:50 +0530

    Correct GPT prompts that had "all" changed to "list" because of refactoring

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -23,7 +23,7 @@ export async function POST(req: Request, res: Response) {
         let questions: any;
         if (type === "open_ended") {
             questions = await strict_output(
-                "You are a helpful AI that is able to generate a pair of question and answers, the length of each answer should not be more than 15 words, store list the pairs of answers and questions in a JSON array",
+                "You are a helpful AI that is able to generate a pair of question and answers, the length of each answer should not be more than 15 words, store all the pairs of answers and questions in a JSON array",
                 new Array(amount).fill(
                     `You are to generate a random hard open-ended questions about ${topic}`
                 ),
@@ -34,7 +34,7 @@ export async function POST(req: Request, res: Response) {
             );
         } else if (type === "mcq") {
             questions = await strict_output(
-                "You are a helpful AI that is able to generate mcq questions and answers, the length of each answer should not be more than 15 words, store list answers and questions and options in a JSON array",
+                "You are a helpful AI that is able to generate mcq questions and answers, the length of each answer should not be more than 15 words, store all answers and questions and options in a JSON array",
                 new Array(amount).fill(
                     `You are to generate a random hard mcq question about ${topic}`
                 ),
